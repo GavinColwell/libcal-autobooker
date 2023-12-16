97fc1c43c1d797ff094ffc6d424f47fec16d6ae9
Gav Colwell <gcolwell828@gmail.com>
2023-12-16 18:21:37 -0500

    use `if ___name__ == "__main__"

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 
 import libcal, json
 
-date = libcal.getDate(days_offset=3)
 
-with open("settings.json") as json_file:
-    settings = json.load(json_file)
-    room = settings["roomId"]
-    startTime = settings["startTime"]
-    numSlots = settings["numSlots"]
+def main(*args):
+    date = libcal.getDate(days_offset=3)
+    
+    with open("settings.json") as json_file:
+        settings = json.load(json_file)
+        room = settings["roomId"]
+        startTime = settings["startTime"]
+        numSlots = settings["numSlots"]
+    
+        libcal.submitBooking(room,date,startTime,numSlots)
 
-    libcal.submitBooking(room,date,startTime,numSlots)
+if __name__ == "__main__":
+    main()
